6e9626a6ce8c804627f0e04a15b5aa617a3b099d
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2025-02-09 09:17:45 +0530

    strucutre of user dashboard

--- Frontend/src/Components/UserDashboard/UserDashboard.jsx
+++ Frontend/src/Components/UserDashboard/UserDashboard.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const UserDashboard = () => {
     return (
         <div>
-            
+            <a href=""><button>Easy</button></a>
+            <a href=""><button>Medium</button></a>
+            <a href=""><button>Difficult</button></a>
         </div>
     );
 };
